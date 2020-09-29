3de6cc984270d1396b0b89457935758d0380a375
nelaaars <nelaaars@gmail.com>
2020-09-29 13:42:35 +0700

    first commit

---
+++ app/src/main/java/com/nelaaars/myintentapp/MoveActivity.kt
@@ -0,0 +1,13 @@
+package com.nelaaars.myintentapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
